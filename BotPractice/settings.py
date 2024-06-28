3558cf6d9fc30377b9a135e4d5bc9fe2e22e0fdc
Nodirxoja Hamidov <nodirxojahamidov@Nodirxojas-MBP.home>
2024-06-28 19:19:06 +0500

    feat(env)

--- BotPractice/settings.py
+++ BotPractice/settings.py
@@ -9,21 +9,27 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-API_TOKEN = "7359985729:AAFyTJd2Ho1l_NaokkwR0eiC2CbPW92wjBY"
+import os
+import environ
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@=y!zmk-@qpk(t+wg!j9@x&q696h928!*!s8^+t=@ame3)b9@l'
+env = environ.Env(
+    DEBUG = (bool, False)
+)
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+BOT_TOKEN = env('API_TOKEN')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
