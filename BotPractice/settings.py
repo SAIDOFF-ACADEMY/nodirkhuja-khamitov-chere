7a33cea258235ee8fa6070bd4b5e857901a99188
Nodirxoja Hamidov <nodirxojahamidov@Nodirxojas-MBP.home>
2024-07-05 17:13:00 +0500

    *

--- BotPractice/settings.py
+++ BotPractice/settings.py
@@ -31,7 +31,7 @@ BOT_TOKEN = env('API_TOKEN')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
